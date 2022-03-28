6c253ed959c88d0b487b097cf9852d4c93ed79e7
주혜민 <>
2022-03-28 21:36:38 +0900

    [add] images

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# yangsan
-양산시 보건소
+# 양산시 보건소
+> #### SITE [link](https://sunminigo.github.io/yangsan/)
---
+++ dist/assets/script/common.js
@@ -0,0 +1,15 @@
+/*
+var Common = function () {
+	var self = this;
+	self.init = function () {
+		console.log('init');
+	}
+	self.init();
+}
+*/
+
+let tab = document.getElementsByClassName('btn_dropdown')
+console.log(tab)
+tab.forEach(function(i) {
+  console.log(i)
+});
